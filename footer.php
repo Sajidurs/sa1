5d7e423ec2d53577618350e75c220c84fd5a64f4
Sajidur <shajidur171@gmail.com>
2021-10-29 22:25:51 +0600

    Second com

---
+++ footer.php
@@ -0,0 +1,59 @@
+
+    <!-- Footer -->
+    <footer class="footer-container d-flex align-items-center">
+        <div class="container">
+            <div class="row align-items-center footer">
+                <div class="col-md-4 text-center text-md-left order-md-1 order-2">
+                    <div class="footer-social">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-linkedin"></i></a>
+                        <a href="#"><i class="fa fa-google"></i></a>
+                        <a href="#"><i class="fa fa-pinterest"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-4 d-flex justify-content-center order-md-2 order-1">
+                    <a href="index.html"><img src="assets/images/logo.png" alt="" class="img-fluid"></a>
+                </div>
+                <div class="col-md-4 order-md-3 order-3">
+                    <div class="footer-cradit text-center text-md-right">
+                        <p>© 2019 <a href="index.html">Themelooks.</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End of Footer -->
+
+    
+    <!-- Back to Top Button -->
+    <div class="back-to-top d-flex align-items-center justify-content-center">
+        <span><i class="fa fa-long-arrow-up"></i></span>
+    </div>
+    <!-- End of Back to Top Button -->   
+
+    
+
+    <!-- JS Files -->
+
+    <!-- ==== JQuery 1.12.1 js file ==== -->
+    <script src="assets/js/jquery-1.12.1.min.js"></script>
+
+    <!-- ==== Bootstrap js file ==== -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <!-- ==== Owl Carousel ==== -->
+    <script src="assets/plugins/owl-carousel/owl.carousel.min.js"></script>
+
+    <!-- ==== Magnific Popup ==== -->
+    <script src="assets/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+     
+    <!-- ==== Script js file ==== -->
+    <script src="assets/js/scripts.js"></script>
+
+    <!-- ==== Custom js file ==== -->
+    <script src="assets/js/custom.js"></script>
+    
+<?php wp_footer();?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * sa1 Theme Function.php Codes
+ * 
+ * @package sa1
+ * Created By: Sajidur Rahman
+ * @since 1.0.0
+ */
+if ( site_url() == "http://localhost.com/sa1" ) {
+    define( "VERSION", time() );
+} else {
+    define( "VERSION", wp_get_theme()->get( "Version" ) );
+}
+function sa1_theme_setup(){
+    load_theme_textdomain( "sa1" );
+    add_theme_support( "post-thumbnails" );
+    add_theme_support( "custom-logo" );
+    add_theme_support( "title-tag" );
+    add_theme_support( 'html5', array( 'search-form', 'comment-list' ) );
+    add_theme_support( "post-formats", array( "image", "gallery", "quote", "audio", "video", "link" ) );
+    add_editor_style( "/assets/css/editor-style.css" );
+
+
+    // Register Nav Menu
+    register_nav_menu( "topmenu", __( "Top Menu", "sa1" ) );
+
+    add_image_size( "sa1_post", 400, 400, true );
+}
+add_action( "after_setup_theme","sa1_theme_setup" );
+
+
+function sa1_assets() {
+    wp_enqueue_style( "fontawesome-css", get_theme_file_uri( "assets/css/font-awesome.min.css" ), null, "1.0" );
+    wp_enqueue_style( "bootstrap-css", get_theme_file_uri( "assets/css/bootstrap.min.css" ), null, "1.0" );
+    wp_enqueue_style( "custom-css", get_theme_file_uri( "assets/css/custom.css" ), null, "1.0" );
+    wp_enqueue_style( "style-css", get_theme_file_uri( "assets/css/style.css" ), null, "1.0" );
+    wp_enqueue_style( "pure-css", get_theme_file_uri( "assets/css/style.css" ), null, "1.0" );
+    
+    // Loading Plugins CSS Files
+    wp_enqueue_style( "fontawesome-css", get_theme_file_uri( "assets/plugins/animate/animate.min.css" ), null, "1.0" );
+    wp_enqueue_style( "fontawesome-css", get_theme_file_uri( "assets/plugins/magnific-popup/magnific-popup.css" ), null, "1.0" );
+    wp_enqueue_style( "fontawesome-css", get_theme_file_uri( "assets/plugins/owl-carousel/owl.carousel.min.css" ), null, "1.0" );
+
+    wp_enqueue_style( "sa1-css", get_stylesheet_uri(), null, VERSION );
+
+
+    // Loading main JS Files
+    wp_enqueue_script( "bootstrap-js", get_theme_file_uri( "assets/js/bootstrap.bundle.min.js" ), null, "1.0" );
+    wp_enqueue_script( "custom-js", get_theme_file_uri( "assets/js/custom.js" ), null, "1.0" );
+    wp_enqueue_script( "jquery-js", get_theme_file_uri( "assets/js/jquery-1.12.1.min.js" ), null, "1.0" );
+    wp_enqueue_script( "scripts-js", get_theme_file_uri( "assets/js/scripts.js" ), array( "jquery" ), "1.0", true );
+
+    // Loading Plugins JS Files
+    wp_enqueue_style( "fontawesome-css", get_theme_file_uri( "assets/plugins/magnific-popup/jquery.magnific-popup.min.js" ), null, "1.0" );
+
+}
+
+add_action( "wp_enqueue_scripts", "sa1_assets" );
